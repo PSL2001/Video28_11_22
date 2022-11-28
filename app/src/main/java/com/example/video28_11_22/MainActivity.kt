263dc267c648e85b1dbf5e38f9902ba704dd7f9e
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-28 13:54:45 +0100

    Comienzo Proyecto: Añadido videos y metodos

--- app/src/main/java/com/example/video28_11_22/MainActivity.kt
+++ app/src/main/java/com/example/video28_11_22/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.video28_11_22
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import com.example.video28_11_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    //Parte 2
+    lateinit var mediaController: MediaController
+    //Parte 3
+    var posicion = 0
+    var rutaVideo = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        //iniciar()
+    }
+
+
+
+    private fun setListeners() {
+        binding.btnPlay.setOnClickListener {
+            reproducirVideo()
+        }
+    }
+
+    private fun reproducirVideo() {
+        //Generamos aleatorio entre 0 y 1
+        var aleatorio = (0 .. 1).random()
+        var idVideo = 0
+        when (aleatorio) {
+            0 -> {
+                idVideo = R.raw.video1
+            }
+            1 -> {
+                idVideo = R.raw.video2
+            }
+        }
+        rutaVideo = "android.resource://"+packageName+"/$idVideo"
+        var uri = Uri.parse(rutaVideo)
+        try {
+            binding.videoView.setVideoURI(uri)
+            binding.videoView.requestFocus()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        //Empezamosel video
+        binding.videoView.start()
     }
 }
